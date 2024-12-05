0438d9df2e63152c78437e967bcf41131d4aeb19
Jack Tang <me@jackhftang.com>
2024-12-05 22:18:09 +0800

    added tryReceiveAsync, receiveAllAsync

--- zmq/asynczmq.nim
+++ zmq/asynczmq.nim
@@ -90,17 +90,7 @@ proc pollAsync*(poller: AsyncZPoller, timeout: int = 2) : Future[int] =
 
   result.complete(r)
 
-proc receiveAsync*(conn: ZConnection): Future[string] =
-  ## Similar to `receive()`, but `receiveAsync()` allows other async tasks to run.
-  ## `receiveAsync()` allows other async tasks to run in those cases.
-  ##
-  ## This will not work in some case because it depends on ZMQ_FD which is not necessarily the 'true' FD of the socket
-  ##
-  ## See https://github.com/zeromq/libzmq/issues/2941 and https://github.com/zeromq/pyzmq/issues/1411
-  let fut = newFuture[string]("receiveAsync")
-  result = fut
-  let sock = conn.socket
-
+template receiveAsyncCallbackTemplate(fut: Future, sock: ZSocket, recv, cb) =
   proc cb(fd: AsyncFD): bool {.closure, gcsafe.} =
     # the cb should work on the low level socket and not the ZConnection object
     result = true
@@ -116,14 +106,44 @@ proc receiveAsync*(conn: ZConnection): Future[string] =
       else:
         # ready to read
         unregister(fd)
-        fut.complete sock.receive(DONTWAIT)
+        fut.complete recv(sock, DONTWAIT)
     except:
       unregister(fd)
       fut.fail getCurrentException()
 
+proc receiveAsync*(conn: ZConnection): Future[string] =
+  ## Similar to `receive()`, but `receiveAsync()` allows other async tasks to run.
+  ## `receiveAsync()` allows other async tasks to run in those cases.
+  ##
+  ## This will not work in some case because it depends on ZMQ_FD which is not necessarily the 'true' FD of the socket
+  ##
+  ## See https://github.com/zeromq/libzmq/issues/2941 and https://github.com/zeromq/pyzmq/issues/1411
+  let fut = newFuture[string]("receiveAsync")
+  result = fut
+  receiveAsyncCallbackTemplate(fut, conn.socket, receive, cb)
   let fd = getsockopt[cint](conn, ZSockOptions.FD).AsyncFD
   register(fd)
   discard cb(fd)
+
+proc tryReceiveAsync*(conn: ZConnection): Future[tuple[msgAvailable: bool, moreAvailable: bool, msg: string]] =
+  ## Async version of `tryReceive()`
+  let fut = newFuture[tuple[msgAvailable: bool, moreAvailable: bool, msg: string]]("tryReceiveAsync")
+  result = fut
+  receiveAsyncCallbackTemplate(fut, conn.socket, tryReceive, cb)
+  let fd = getsockopt[cint](conn, ZSockOptions.FD).AsyncFD
+  register(fd)
+  discard cb(fd)
+
+proc receiveAllAsync*(conn: ZConnection): Future[seq[string]] {.async.} =
+  ## async version for `receiveAll()`
+  var expectMessage = true
+  while expectMessage:
+    let (msgAvailable, moreAvailable, msg) = await tryReceiveAsync(conn)
+    if msgAvailable:
+      result.add msg
+      expectMessage = moreAvailable
+    else:
+      expectMessage = false
 
 proc sendAsync*(conn: ZConnection, msg: string, flags: ZSendRecvOptions = DONTWAIT): Future[void] =
   ## `send()` is blocking for some connection types (e.g. PUSH, DEALER).
@@ -134,11 +154,11 @@ proc sendAsync*(conn: ZConnection, msg: string, flags: ZSendRecvOptions = DONTWA
   ## See https://github.com/zeromq/libzmq/issues/2941 and https://github.com/zeromq/pyzmq/issues/1411
   let fut = newFuture[void]("sendAsync")
   result = fut
-  let sock = conn.socket
 
   let status = getsockopt[cint](conn, ZSockOptions.EVENTS)
   if (status and ZMQ_POLLOUT) == 0:
     # wait until queue available
+    let sock = conn.socket
     proc cb(fd: AsyncFD): bool {.closure, gcsafe.} =
       result = true
 
